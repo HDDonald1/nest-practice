7ecb042aa0bec178acd356ce8b71092d0b349583
Myroslav Strukov <miroslavstrukov3@gmail.com>
2022-08-24 22:17:55 +0300

    add mongoose and create cats scheme

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://user1:VqmecRVMcISc1UsK@cluster0.etk5jqu.mongodb.net/cats?retryWrites=true&w=majority`),
+    CatsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
